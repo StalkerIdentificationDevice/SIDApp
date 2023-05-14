8a8bc3a9ea361eb3fa9dc104273653d16bd97523
bgolden1 <brodygolden7@gmail.com>
2023-05-14 12:16:00 -0400

    Use webview to serve video

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
-import { useWindowDimensions, StyleSheet } from 'react-native';
+import { View, StyleSheet, Button, useWindowDimensions, Text } from 'react-native';
 import * as React from 'react';
 import RenderHtml from 'react-native-render-html';
+import { WebView } from 'react-native-webview';
 
 const source = {
   html: `
-  <img src='http://172.20.10.4:8000/index.html' width="640" height="480"/>`
+  <body>
+<h1>Stalker Identification Device</h1>
+<img src="http://172.20.10.4:8000/stream.mjpg" width="640" height="480">
+</body>`
 };
 
 export default function App() {
   const { width } = useWindowDimensions();
   return (
-    <RenderHtml source={source} contentWidth={width}/>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to SID</Text>
+      <Text style={styles.body}>Live feed</Text>
+      <WebView source={{uri: 'http://172.20.10.4:8000/stream.mjpg'}}/>
+    </View>
   );
 }
 
@@ -18,7 +26,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 50,
   },
+  title: {
+    fontSize: 30,
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    margin: 10
+  },
+  body: {
+    alignSelf: 'center'
+  }
 });
