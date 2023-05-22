63beb791265ef9c5f1041bbb1ad2a834fbc42daf
bgolden1 <brodygolden7@gmail.com>
2023-05-22 00:19:41 -0400

    async task additon

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import { View, StyleSheet, Text, Switch, Button, TouchableOpacity } from 'react-
 import * as React from 'react';
 import { WebView } from 'react-native-webview';
 import { useState } from 'react';
-import * as Linking from "expo-linking"
+import * as Linking from "expo-linking";
+import * as BackgroundFetch from 'expo-background-fetch';
+import * as TaskManager from 'expo-task-manager';
 
 export default function App() {
   const [isArmed, setArmed] = useState(true)
@@ -10,20 +12,21 @@ export default function App() {
   const [contactPhone, setContactPhone] = useState("+18502522527")
   function switchCallback () {
     if(isArmed) {
-      fetch('http://172.20.10.4:8000/disarm', {method: "GET"}).then(() => {
+      fetch('http://raspberrypi.local:8000/disarm', {method: "GET"}).then(() => {
         setArmed(false)
       }).catch((err) => {
         console.error(err);
       })
     }
     else {
-      fetch('http://172.20.10.4:8000/arm', {method: "GET"}).then(() => {
+      fetch('http://raspberrypi.local:8000/arm', {method: "GET"}).then(() => {
         setArmed(true)
       }).catch((err) => {
         console.error(err);
       })
     }
   }
+  
   function onCallPress() {
     Linking.openURL(`tel:${contactPhone}`).then(() => {
       console.log(`Successfully called ${contactPhone}`)
@@ -31,11 +34,21 @@ export default function App() {
       console.error(err)
     })
   }
+
+  TaskManager.defineTask("get_shot", async () => {
+    await fetch('http://raspberrypi.local:8000/shot', {method: "GET"})
+    return BackgroundFetch.BackgroundFetchResult.NewData;
+  })
+
+  BackgroundFetch.registerTaskAsync("get_shot", {
+    minimumInterval: 30
+  });
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to SID</Text>
       <Text style={styles.body}>Live feed</Text>
-      {isArmed ? <WebView style={styles.video} source={{uri: 'http://172.20.10.4:8000/stream.mjpg'}}/> : <View style={styles.disarm}><Text style={styles.body}>The camera is turned off</Text></View>}
+      {isArmed ? <WebView style={styles.video} source={{uri: 'http://raspberrypi.local:8000/stream.mjpg'}}/> : <View style={styles.disarm}><Text style={styles.body}>The camera is turned off</Text></View>}
       {isArmed ? <Text style={styles.body}>Armed</Text> : <Text style={styles.body}>Disarmed</Text>}
       <Switch style={styles.switch} onValueChange={switchCallback} value={isArmed}/>
       <TouchableOpacity style={styles.call_button} onPress={onCallPress}>
