abd2a1c1e3a16b9675cee2b88cfd14d83dfcfb98
bgolden1 <brodygolden7@gmail.com>
2023-07-18 19:59:09 -0400

    add notifications and circleci config

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { View, StyleSheet, Text, Switch, Button, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Text, Switch, ActivityIndicator, TouchableOpacity } from 'react-native';
 import * as React from 'react';
 import { WebView } from 'react-native-webview';
 import { useState, useEffect, useRef } from 'react';
@@ -6,6 +6,16 @@ import * as Linking from "expo-linking";
 import { Amplify } from 'aws-amplify';
 import awsconfig from './src/aws-exports';
 import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react-native';
+import * as Notifications from 'expo-notifications';
+import * as Device from 'expo-device';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 // >>New - Configuring Auth Module
 Amplify.configure(awsconfig);
@@ -48,9 +58,34 @@ function AuthenticatedApp() {
 function App() {
   const [isArmed, setArmed] = useState(false)
   const { user, signOut } = useAuthenticator();
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const [isLoading, setLoading] = useState(false)
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => {
+      setExpoPushToken(token);
+    });
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
 
 
   function switchCallback() {
+    setLoading(true)
     if (isArmed) {
       fetch('http://raspberrypi.local:8000/disarm', { method: "GET" }).then(() => {
         setArmed(false)
@@ -59,12 +94,13 @@ function App() {
       })
     }
     else {
-      fetch('http://raspberrypi.local:8000/arm', { method: "GET", headers: {'username': user.username, 'Cache-Control': 'no-cache'} }).then((res) => {
+      fetch('http://raspberrypi.local:8000/arm', { method: "GET", headers: {'username': user.username, 'device_token': expoPushToken} }).then(() => {
         setArmed(true)
       }).catch((err) => {
         console.error(err);
       })
     }
+    setLoading(false)
   }
 
   function onCallPress() {
@@ -91,8 +127,12 @@ function App() {
       <Text style={styles.title}>Welcome to SID</Text>
       <Text style={styles.body}>Live feed</Text>
       {isArmed ? <WebView style={styles.video} source={{ uri: 'http://raspberrypi.local:8000/stream.mjpg' }} /> : <View style={styles.disarm}><Text style={styles.body}>The camera is turned off</Text></View>}
-      {isArmed ? <Text style={styles.body}>Armed</Text> : <Text style={styles.body}>Disarmed</Text>}
-      <Switch style={styles.switch} onValueChange={switchCallback} value={isArmed} />
+      {isLoading ? <ActivityIndicator/> : 
+      <View>
+        {isArmed ? <Text style={styles.body}>Armed</Text> : <Text style={styles.body}>Disarmed</Text>}
+        <Switch style={styles.switch} onValueChange={switchCallback} value={isArmed} />
+      </View>
+      }
       <View style={{flexDirection: 'row', alignSelf: "center", alignContent: "space-around"}}>
       <TouchableOpacity style={styles.call_button} onPress={onCallPress}>
         <Text style={styles.call_text}>Call {user.attributes['custom:emergency-contact']}</Text>
@@ -104,6 +144,38 @@ function App() {
       
     </View>
   );
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+    fetch('')
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
 }
 
 const styles = StyleSheet.create({
